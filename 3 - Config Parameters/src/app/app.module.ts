f53ba33b24277121fa4ce3bf1613a45e8a8640c8
Chris Griffith <chris.griffith@gmail.com>
2019-02-27 17:30:59 -0800

    Demo Updates

--- 3 - Config Parameters/src/app/app.module.ts
+++ 3 - Config Parameters/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(
+    {
+      mode: 'ios',
+      tabButtonLayout: 'icon-start',
+      spinner: 'bubbles'
+    }
+  ), AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
